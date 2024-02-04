5b76d4fdb236a67c731bcef351b14db6ab0f7874
arinapras <116898611+arinapras@users.noreply.github.com>
2024-02-04 14:54:49 +0800

    test

---
+++ PE07_grp4_Project/Server/Data/Migrations/20240129024711_AddApplicationTables.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PE07_grp4_Project.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organisers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    organiserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    organiserContact = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organisers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    eventName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    eventDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    eventLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrganiserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Organisers_OrganiserId",
+                        column: x => x.OrganiserId,
+                        principalTable: "Organisers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganiserId",
+                table: "Events",
+                column: "OrganiserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Organisers");
+        }
+    }
+}
---
+++ PE07_grp4_Project/Server/Data/Migrations/20240203172937_AddedDefaultDataAndUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PE07_grp4_Project.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultDataAndUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3781efa7-66dc-47f0-860f-e506d04102e4",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5baf7aa0-5212-4883-9542-ea7610e8a44a", "AQAAAAIAAYagAAAAEGal7RpwAt/I09+OEmZ8NbYL52GFzgKj25yjV1EGIso0H4Tpxl8KqjpKKBorMi4b7w==", "ead94bfa-1fea-4f99-84bb-281a74e1635d" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3781efa7-66dc-47f0-860f-e506d04102e4",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "968bc54a-dda1-416d-bf8a-80b68a88764a", "AQAAAAIAAYagAAAAEFQLmJ1ieolRZBoW4D6drNvZUykhMI/RaGwfQZlyjxGs0ezC/oB4f9xpTCb5BWnC2A==", "9ac90919-3936-471d-88a3-6d703dbb7590" });
+        }
+    }
+}
